b2f50174dd8afa24a8c14b336a003206a3ffeb5e
dotis90 <dotis90@gmail.com>
2022-06-16 08:26:36 -0600

    End of video 44 Additive Scene Loading

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -17,6 +17,9 @@ public class GameController : MonoBehaviour
 
     GameState stateBeforePause;
 
+    public SceneDetails CurrentScene { get; private set; }
+    public SceneDetails PrevScene { get; private set; }
+
     public static GameController Instance { get; private set; }
 
     private void Awake()
@@ -116,5 +119,11 @@ public class GameController : MonoBehaviour
         {
             DialogueManager.Instance.HandleUpdate();
         }
+    }
+
+    public void SetCurrentScene(SceneDetails currScene)
+    {
+        PrevScene = CurrentScene;
+        CurrentScene = currScene;
     }
 }
