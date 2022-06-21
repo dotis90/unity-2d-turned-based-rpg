0537115762b85782d361a51bbbf0f1f21093a557
dotis90 <dotis90@gmail.com>
2022-06-21 11:04:51 -0600

    End of video 52 Creating Inventory System

---
+++ Assets/Scripts/Inventory/ItemBase.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBase : ScriptableObject
+{
+    [SerializeField] string _name;
+    [SerializeField] string description;
+    [SerializeField] Sprite icon;
+
+    public string Name => _name;
+    public string Description => description;
+    public Sprite Icon => icon;
+}
