816e4f6647d03d6e4e4b9a6f35798245ea48e15c
dotis90 <dotis90@gmail.com>
2022-07-05 10:53:27 -0600

    End of video 60 Using Pokeball Items From Inventory

--- Assets/Scripts/Inventory/UI/InventoryUI.cs
+++ Assets/Scripts/Inventory/UI/InventoryUI.cs
@@ -20,7 +20,7 @@ public class InventoryUI : MonoBehaviour
 
     [SerializeField] PartyScreen partyScreen;
 
-    Action onItemUsed;
+    Action<ItemBase> onItemUsed;
 
     int selectedItem = 0;
     int selectedCategory = 0;
@@ -68,7 +68,7 @@ public class InventoryUI : MonoBehaviour
         UpdateItemSelection();
     }
 
-    public void HandleUpdate(Action onBack, Action onItemUsed=null)
+    public void HandleUpdate(Action onBack, Action<ItemBase> onItemUsed=null)
     {
         this.onItemUsed = onItemUsed;
 
@@ -120,7 +120,7 @@ public class InventoryUI : MonoBehaviour
 
             if (Input.GetKeyDown(KeyCode.Z))
             {
-                OpenPartyScreen();
+                ItemSelected();
             }
             else if (Input.GetKeyDown(KeyCode.X))
             {
@@ -145,6 +145,18 @@ public class InventoryUI : MonoBehaviour
         
     }
 
+    void ItemSelected()
+    {
+        if (selectedCategory == (int)ItemCategory.Pokeballs)
+        {
+            StartCoroutine(UseItem());
+        }
+        else
+        {
+            OpenPartyScreen();
+        }
+    }
+
     IEnumerator UseItem()
     {
         state = InventoryUIState.Busy;
@@ -152,8 +164,12 @@ public class InventoryUI : MonoBehaviour
         var usedItem = inventory.UseItem(selectedItem, partyScreen.SelectedMember, selectedCategory);
         if(usedItem != null)
         {
-            yield return DialogueManager.Instance.ShowDialogText($"The player used { usedItem.Name }.");
-            onItemUsed?.Invoke();
+            if (!(usedItem is PokeballItem))
+            {
+                yield return DialogueManager.Instance.ShowDialogText($"The player used { usedItem.Name }.");
+            }
+            
+            onItemUsed?.Invoke(usedItem);
         }
         else
         {
