1fe2d188940ad239afbebc89e53815ca333353b4
dotis90 <dotis90@gmail.com>
2022-07-09 11:03:39 -0600

    End of Video 80 Shops 2 Money

--- Assets/Scripts/Items/ShopController.cs
+++ Assets/Scripts/Items/ShopController.cs
@@ -8,6 +8,7 @@ public enum ShopState { Menu, Buying, Selling, Busy }
 public class ShopController : MonoBehaviour
 {
     [SerializeField] InventoryUI inventoryUI;
+    [SerializeField] WalletUI walletUI;
 
     public event Action OnStart;
     public event Action OnFinish;
@@ -85,6 +86,8 @@ public class ShopController : MonoBehaviour
             yield break;
         }
 
+        walletUI.Show();
+
         float sellingPrice = Mathf.Round(item.Price / 2);
 
         int selectedChoice = 0;
@@ -96,10 +99,12 @@ public class ShopController : MonoBehaviour
         {
             // Yes
             inventory.RemoveItem(item);
-            // TODO: Add selling price to player's wallet
+            Wallet.i.AddMoney(sellingPrice);
             yield return DialogueManager.Instance.ShowDialogText($"Sold { item.Name } for { sellingPrice} money.");
 
         }
+
+        walletUI.Close();
 
         state = ShopState.Selling;
     }
