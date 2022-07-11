0e91e2f7e6af206110dd3ffaed524e915123b1dc
dotis90 <dotis90@gmail.com>
2022-07-11 12:23:00 -0600

    End of Video 82 Shops 4 Shop UI

---
+++ Assets/Scripts/UI/ShopUI.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopUI : MonoBehaviour
+{
+    [SerializeField] GameObject itemList;
+    [SerializeField] ItemSlotUI itemSlotUI;
+
+    [SerializeField] Image itemIcon;
+    [SerializeField] Text itemDescription;
+
+    [SerializeField] Image UpArrow;
+    [SerializeField] Image DownArrow;
+
+    int selectedItem;
+
+    List<ItemBase> availableItems;
+
+    List<ItemSlotUI> slotUIList;
+
+    const int itemsInViewport = 8;
+
+    RectTransform itemListRect;
+
+    private void Awake()
+    {
+        itemListRect = itemList.GetComponent<RectTransform>();
+    }
+
+    public void Show(List<ItemBase> availableItems)
+    {
+        this.availableItems = availableItems;
+
+        gameObject.SetActive(true);
+
+        UpdateItemList();
+
+    }
+
+    public void HandleUpdate()
+    {
+        var prevSelection = selectedItem;
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            ++selectedItem;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            --selectedItem;
+        }
+
+        selectedItem = Mathf.Clamp(selectedItem, 0, availableItems.Count - 1);
+
+        if (selectedItem != prevSelection)
+        {
+            UpdateItemSelection();
+        }
+    }
+
+    void UpdateItemList()
+    {
+        // Clear all the existing items
+        foreach (Transform child in itemList.transform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        slotUIList = new List<ItemSlotUI>();
+        foreach (var item in availableItems)
+        {
+            var slotUIObj = Instantiate(itemSlotUI, itemList.transform);
+            slotUIObj.SetNameAndPrice(item);
+
+            slotUIList.Add(slotUIObj);
+        }
+
+        UpdateItemSelection();
+    }
+
+    void UpdateItemSelection()
+    {
+
+        selectedItem = Mathf.Clamp(selectedItem, 0, availableItems.Count - 1);
+
+        for (int i = 0; i < slotUIList.Count; i++)
+        {
+            if (i == selectedItem)
+            {
+                slotUIList[i].NameText.color = GlobalSettings.i.HighlightedColor;
+            }
+            else
+            {
+                slotUIList[i].NameText.color = Color.black;
+            }
+        }
+
+        if (availableItems.Count > 0)
+        {
+            var item = availableItems[selectedItem];
+            itemIcon.sprite = item.Icon;
+            itemDescription.text = item.Description;
+        }
+
+        HandleScrolling();
+    }
+
+    void HandleScrolling()
+    {
+        if (slotUIList.Count <= itemsInViewport)
+        {
+            return;
+        }
+
+        float scrollPos = Mathf.Clamp(selectedItem - itemsInViewport / 2, 0, selectedItem) * slotUIList[0].Height;
+        itemListRect.localPosition = new Vector2(itemListRect.localPosition.x, scrollPos);
+
+        bool showUpArrow = selectedItem > itemsInViewport / 2;
+        UpArrow.gameObject.SetActive(showUpArrow);
+
+        bool showDownArrow = selectedItem + itemsInViewport / 2 < slotUIList.Count;
+        DownArrow.gameObject.SetActive(showDownArrow);
+    }
+}
