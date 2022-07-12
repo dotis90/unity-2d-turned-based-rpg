c0e2723b9a59e042ac338dca1351345854eb596c
dotis90 <dotis90@gmail.com>
2022-07-12 13:31:31 -0600

    End of Video 89 Improving Wild Encounters

--- Assets/Scripts/Gameplay/MapArea.cs
+++ Assets/Scripts/Gameplay/MapArea.cs
@@ -1,27 +1,45 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class MapArea : MonoBehaviour
 {
-    [SerializeField] List<Pokemon> wildPokemon;
+    [SerializeField] List<PokemonEncounterRecord> wildPokemons;
+
+    void Start()
+    {
+        int totalChance = 0;
+        foreach(var record in wildPokemons)
+        {
+            record.chanceLower = totalChance;
+            record.chanceUpper = totalChance + record.chancePercentage;
+
+            totalChance = totalChance + record.chancePercentage;
+        }
+    }
 
     public Pokemon GetWildPokemon()
     {
-        var pokemon = wildPokemon[Random.Range(0, wildPokemon.Count)];
-        pokemon.Init();
-        return pokemon;
-    }
+        int randVal = Random.Range(1, 101);
+        var pokemonRecord = wildPokemons.First(p => randVal >= p.chanceLower && randVal <= p.chanceUpper);
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+        var levelRange = pokemonRecord.levelRange;
+        int level = levelRange.y == 0 ? levelRange.x : Random.Range(levelRange.x, levelRange.y + 1);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        var wildPokemon = new Pokemon(pokemonRecord.pokemon, level);
+        wildPokemon.Init();
+        return wildPokemon;
     }
+}
+
+[System.Serializable]
+public class PokemonEncounterRecord
+{
+    public PokemonBase pokemon;
+    public Vector2Int levelRange;
+    public int chancePercentage;
+
+    public int chanceLower { get; set; }
+    public int chanceUpper { get; set; }
 }
